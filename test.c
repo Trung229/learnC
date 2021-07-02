6ce0500edb6ca2d12360318e8554dcd3e0b527cd
Trung <ductrung4263@gmail.com>
2021-07-02 08:47:57 +0700

    practice print name - sutdentID - email - phone - address use method gets and fflush

--- test.c
+++ test.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 
 int main(){
-    char name;
+    char name[20];
+    char idStudent[10];
+    char email[20];
+    int phone;
+    char address[100];
     printf("mời bạn nhập tên: ");
     gets(name);
-    printf("Hello %s", name);
+    fflush(stdin);
+    printf("mời bạn nhập MSSV: ");
+    gets(idStudent);
+    printf("mời bạn nhập email: ");
+    gets(email);
+    printf("mời bạn nhập phone: ");
+    scanf("%d",&phone);
+    fflush(stdin);
+    printf("mời bạn nhập địa chỉ: ");
+    gets(address);
+    printf("\nHello %s \n", name);
+    printf("thông tin của bạn là\n");
+    printf("Name: %s\n", name);
+    printf("ID: %s\n", idStudent);
+    printf("Phone: %d\n", phone);
+    printf("Address: %s\n", address);
     return 0;
 }
